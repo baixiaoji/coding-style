aa66eb645daf90c29a901ee256921bc75930a716
baixiaoji <1041874421@qq.com>
2019-11-18 17:16:34 +0800

    :art: 添加了prettier的信息，支持ts的eslint

--- src/sum.js
+++
@@ -1,9 +0,0 @@
-function sum(a, b) {
-	return a + b
-}
-var bool = false;
-if (!bool) {
-	console.log("testing");
-}
-console.log(global);
-export default sum
---
+++ src/sum.ts
@@ -0,0 +1,10 @@
+function sum(a: number, b: number): number {
+  return a + b;
+}
+var bool: boolean = false;
+
+if (!bool) {
+  console.log("testing");
+}
+
+sum(1, 2);
